03fffcd4edc3dd8dd4957b1b92d723c7e0a727bb
blumenkratz <blumenkratz@blumenkratz-VB>
2020-08-16 20:59:18 +0700

    final

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main()
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
     std::cout<<"this is mad scientist"<<std::endl;
+    int g=6;
+    std::cout<<g<<std::endl;
 
     return 0;
 }
